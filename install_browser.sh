e8fe8db6f0c4925f3ad4e31311c4af4863d03ae6
adadiehl <adadiehl@umich.edu>
2016-08-05 13:59:25 -0400

    Added restart option to keep containers alive in the event of failure

--- install_browser.sh
+++ install_browser.sh
@@ -16,7 +16,7 @@ cd MouseSOM/sql
 tar -xzf sql_data.tgz
 cd $WD
 echo "Setting up database container..."
-docker run -d --name db -e MYSQL_ROOT_PASSWORD=my-secret-pw -v $BROWSER_PATH/sql/:/data/sql mysql:5.7.13
+docker run -d --name db -e MYSQL_ROOT_PASSWORD=my-secret-pw -v $BROWSER_PATH/sql/:/data/sql mysql:5.7.13 --restart unless-stopped
 #docker run -it --link db -v $BROWSER_PATH/sql/:/data/sql --rm mysql sh -c 'exec mysql -h 172.18.0.2 -P 3306 -uroot -pmy-secret-pw </data/sql/MouseSOM.sql'
 #docker run -it --link db -v $BROWSER_PATH/sql/:/data/sql --rm mysql sh -c 'exec mysql -h 172.18.0.2 -P 3306 -uroot -pmy-secret-pw mousesom </data/sql/MouseSOM_DB_DATA.sql'
 # Users.sql sets up the 'webuser' account and changes the root password. The
@@ -28,13 +28,17 @@ docker run -d --name db -e MYSQL_ROOT_PASSWORD=my-secret-pw -v $BROWSER_PATH/sql
 # -p 5000:80 is for the production server. Change the outward-facing port (5000) to match your local
 # configuration.
 echo "Setting up MouseSOM Browser container..."
-docker run -t --name="MouseSOM" --link db -v $BROWSER_PATH/:/var/www/MouseSOM/ -d -p 3000:3000 -p 5000:80 adadiehl/browser
+docker run -t --name="MouseSOM" --link db -v $BROWSER_PATH/:/var/www/MouseSOM/ -d -p 3000:3000 -p 5000:80 adadiehl/browser --restart unless-stopped
 docker exec -i MouseSOM sh -c 'cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig'
 docker exec -i MouseSOM sh -c 'cat > /etc/nginx/nginx.conf' <nginx.conf
 
 # The following should not be necessary for most applications.
 #docker exec -i MouseSOM sh -c 'cp /etc/nginx/fastcgi_params /etc/nginx/fastcgi_params.orig'
 #docker exec -i MouseSOM sh -c 'cat > /etc/nginx/fastcgi_params' <fastcgi_params
+
+# Optional: set up monit process monitor to keep browser services running
+docker exec -i MouseSOM sh -c 'cat > /etc/monitrc' <monitrc
+docker exec -i MouseSOM sh -c 'chmod 0700 /etc/monitrc'
 
 # Load the database data
 docker run -it --link db -v $BROWSER_PATH/sql/:/data/sql --rm mysql sh -c 'exec mysql -h 172.18.0.2 -P 3306 -uroot -pmy-secret-pw </data/sql/MouseSOM.sql'
